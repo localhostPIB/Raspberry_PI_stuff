bf061e90ed75c1da738e1e7d98bd6e00927d317c
localhostPIB <nurschule@gmail.com>
2022-10-23 19:53:11 +0200

    Create cheat_sheet.sh

---
+++ cheat_sheet.sh
@@ -0,0 +1,39 @@
+# Update und Upgrade:
+sudo apt-get update && sudo apt-get upgrade
+
+# install cURL
+sudo apt-get install curl
+
+# wget
+wget https://raw.githubusercontent.com/localhostPIB/Raspberry_PI_stuff/main/pgadmin_install.sh
+
+# Install Apache 2:
+sudo apt install apache2
+
+# Apache 2 Version:
+apache2 -version
+
+# Apache 2 Firewall configuration:
+sudo ufw app list
+
+# Restart PostgreSQL:
+sudo service postgresql restart
+
+# Restart Apache 2:
+sudo service apache2 restart
+
+# Turn Wi-Fi off:
+sudo nmcli radio wifi off
+
+# Temp:
+sensors
+
+# Task-Manager:
+htop
+
+# Reboot:
+sudo reboot
+
+# Shutdown:
+sudo shutdown
+
