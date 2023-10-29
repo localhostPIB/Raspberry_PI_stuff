0e337e5624a876180a2a347c49386cda456767c9
localhostPIB <nurschule@gmail.com>
2023-10-29 14:47:32 +0100

    Update cheat_sheet.sh
    docker, docker compose and portainer

--- cheat_sheet.sh
+++ cheat_sheet.sh
@@ -61,7 +61,23 @@ sudo shutdown -h now
  pip install <foo>
  deactivate
 
+# install docker and curl
+sudo apt install docker curl -y
 
+# install docker compose  (v2.23.0)
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+
+# chmod docker compose for portainer
+sudo chmod +x /usr/local/bin/docker-compose    
+
+# create the volume that Portainer Server will use to store its database
+sudo docker volume create portainer_data
+
+# download and install  Portainer Server container
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+# restart Portainer
+sudo docker restart portainer
 
 # Install Git
 sudo apt install git
